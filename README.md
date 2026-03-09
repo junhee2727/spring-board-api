82e7bfeaf50cb03cb846bb97fb8156387335d768
junheeLee <junhee2727@gmail.com>
2026-03-09 11:16:43 +0900

    Spring Boot project init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spring Board API
+
+Spring Boot 기반 게시판 API 서버 프로젝트입니다.
+
+## 기술 스택
+- Java
+- Spring Boot
+- Spring Data JPA
+- MySQL
+
+## 목표
+백엔드 취업 준비를 위한 프로젝트
---
+++ src/main/java/com/junhee/spring_board_api/SpringBoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.junhee.spring_board_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoardApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoardApiApplication.class, args);
+	}
+
+}
